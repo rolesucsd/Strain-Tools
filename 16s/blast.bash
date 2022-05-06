d08e9bd867d67a607d3275c5429884e0265ab6d8
rolesucsd <93601015+rolesucsd@users.noreply.github.com>
2022-05-06 11:35:44 -0700

    Update blast.bash

--- 16s/blast.bash
+++ 16s/blast.bash
@@ -2,6 +2,8 @@
 
 source ~/opt/anaconda3/bin/activate
 python blast.py
+conda activate blast
+makeblastdb -in ncbi/sequence.fasta -parse_seqids -blastdb_version 5 -title "sequence" -dbtype nucl
 for f in files/*.fastq
 do
 	f2=$(basename -s .fastq $f)
@@ -9,6 +11,6 @@ do
 	seqtk trimfq -l 700 $f > files/$f2.trim.fq
 	seqtk seq -a files/$f2.trim.fq > files/$f2.fasta
 	conda activate blast
-	blastn -db ncbi/sequence.fasta -query files/$f2.fasta -qcov_hsp_perc 90 -perc_identity 95 -max_target_seqs 3 -out files/$f2.out -evalue 1e-6 -num_threads 4 -outfmt '6'
+	blastn -db ncbi/sequence.fasta -query files/$f2.fasta -qcov_hsp_perc 50 -perc_identity 50 -max_target_seqs 3 -out files/$f2.out -evalue 1e-6 -num_threads 4 -outfmt '6'
 done
 Rscript 16s.R
