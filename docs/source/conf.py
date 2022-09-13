98f70a33dac2e0e2e9c227866155160c2f256ac7
Renee Oles <93601015+rolesucsd@users.noreply.github.com>
2022-09-13 12:41:17 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,34 +11,61 @@ copyright = '2022, Renee Oles'
 author = 'Renee Oles'
 release = 'Sep 2022'
 
+import sphinx_rtd_theme
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
+# The master toctree document.
+master_doc = 'index'
+
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+  'display_version': False,
+  'navigation_depth': 2,
+  'collapse_navigation': True
 }
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_last_updated_fmt = '%b %d, %Y'
+html_context = {
+  'display_github': True,
+  'github_user': 'Uberspace',
+  'github_repo': 'manual',
+  'github_version': 'main',
+  'conf_py_path': '/source/',
+  'changelog_entries': changelog_entries,
+  'newest_changelog_entry': changelog_entries[0],
+}
+html_show_copyright = False
+html_favicon = '_static/favicon.ico'
+html_css_files = [
+    'css/custom.css',
+]
+html_static_path = ['_static']
+
+
+html_sidebars = {
+    '**': [
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+    ]
+}
+
+
+
